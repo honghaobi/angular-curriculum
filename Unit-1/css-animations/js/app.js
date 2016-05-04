e8a1df3e7d94fe68e46070e4374fc8fe25d214f9
henrybi <henrybi@u.washington.edu>
2016-05-04 11:02:04 -0700

    added css-animation

---
+++ Unit-1/css-animations/js/app.js
@@ -0,0 +1,7 @@
+$(document).on('ready', function() {
+  var w = window.innerWidth,
+      h = window.innerHeight,
+      headSpace = (h - 300)/2;
+  $('.wrapper').css('min-height', h);
+  $('.container').css('padding-top', headSpace);
+});
