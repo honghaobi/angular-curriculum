e4253828a5c897c0f7d6b80a76a0a9dc4c7929cf
henrybi <henrybi@u.washington.edu>
2016-05-09 18:11:16 -0700

    regular routes done

---
+++ Unit-2/05routes/app.js
@@ -0,0 +1,13 @@
+angular.module("app", ['ngRoute'])
+.config(function($routeProvider){
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    })
+    .when('/dogs', {
+      templateUrl: 'partials/dogs.html',
+      controller: 'DogController'
+    })
+});
---
+++ Unit-2/05routes/controller.js
@@ -0,0 +1,5 @@
+angular.module("app").controller('HomeController', function($scope){
+  $scope.home = 'henrys home';
+}).controller('DogController', function($scope){
+  $scope.dog = 'Jay';
+})
