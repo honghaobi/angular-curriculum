ba4f69e17682091108df4991d225914745502520
henrybi <henrybi@u.washington.edu>
2016-05-09 21:35:00 -0700

    bootstrap with angular menu active class

--- Unit-2/05routes/app.js
+++ Unit-2/05routes/app.js
@@ -10,6 +10,18 @@ angular.module("app", ['ngRoute'])
       templateUrl: 'partials/dogs.html',
       controller: 'DogController'
     })
+    .when('/projects', {
+      templateUrl: 'partials/projects.html',
+      controller: 'ProjectController'
+    })
+    .when('/bio', {
+      templateUrl: 'partials/bio.html',
+      controller: 'BioController'
+    })
+    .when('/resume', {
+      templateUrl: 'partials/resume.html',
+      controller: 'ResumeController'
+    })
     .otherwise({
       redirectTo: '/'
     });
--- Unit-2/05routes/controller.js
+++ Unit-2/05routes/controller.js
@@ -2,4 +2,11 @@ angular.module("app").controller('HomeController', function($scope){
   $scope.home = 'henrys home';
 }).controller('DogController', function($scope){
   $scope.dog = 'Jay';
-})
+}).controller('BioController', function($scope){
+}).controller('ProjectController', function($scope){
+}).controller('ResumeController', function($scope){
+}).controller('HeaderController', function($scope, $location){
+  $scope.isActive = function (viewLocation) {
+     return viewLocation === $location.path();
+  };
+});
