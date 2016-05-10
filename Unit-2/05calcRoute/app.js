7d53c5bc74ba831c555346cf6c000676bb7380b9
henrybi <henrybi@u.washington.edu>
2016-05-09 22:21:28 -0700

    routes calc 2 query done

--- Unit-2/05calcRoute/app.js
+++ Unit-2/05calcRoute/app.js
@@ -1,18 +1,36 @@
 angular.module("app", ['ngRoute'])
 .config(function($routeProvider,$locationProvider){
   $routeProvider
-    .when('/:num1/:operator/:num2', {
+    .when('/:operator/:num1/:num2', {
       templateUrl: 'calc.html',
-      controller: 'CalcController'
+      controller: 'CalcParamsController'
+    }).when('/:operator/?', {
+      templateUrl: 'calc.html',
+      controller: 'CalcQueryController'
     });
 })
-.controller('CalcController', function($scope,$routeParams){
+.controller('CalcParamsController', function($scope, $routeParams){
     var operator = {
       add: "+",
       subtract: "-",
       multiply: "*",
       divide: "/"
     };
-    
+
     $scope.answer = eval($routeParams.num1 + operator[$routeParams.operator] + $routeParams.num2);
+})
+.controller('CalcQueryController', function($scope, $location, $routeParams){
+
+    if ( $location.search().hasOwnProperty( 'x' ) && $location.search().hasOwnProperty( 'y' )) {
+     var x = $location.search()['x'];
+     var y = $location.search()['y'];
+  }
+    var operator = {
+      add: "+",
+      subtract: "-",
+      multiply: "*",
+      divide: "/"
+    };
+
+    $scope.answer = eval(x + operator[$routeParams.operator] + y);
 });
