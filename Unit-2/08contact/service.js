d118105f646bd82140be57dbe7d84201568e1767
henrybi <henrybi@u.washington.edu>
2016-05-12 01:33:57 -0700

    implemented gif feature

--- Unit-2/08contact/service.js
+++ Unit-2/08contact/service.js
@@ -5,13 +5,21 @@
         .module("app")
         .service("ContactList", ContactList);
 
-    function ContactList() {
+    function ContactList($http) {
       var ContactList = {}
 
-      ContactList.contactList = [{name:"henry", email:"henry@google.com"}, {name:"steve", email:"steve@apple.com"}];
+      ContactList.contactList = [];
 
       ContactList.addContact = function(obj){
         ContactList.contactList.push(obj);
+        ContactList.getGif(obj.name);
+      };
+      ContactList.getGif = function(name){
+        $http.get(`http://api.giphy.com/v1/gifs/search?q=${name}&api_key=dc6zaTOxFJmzC`)
+          .then(function(data){
+            ContactList.contactList[ContactList.contactList.length - 1].gif = data.data.data[0].images.downsized.url;
+            console.log(ContactList.contactList);
+        });
       };
       ContactList.removeContact = function(name){
         var contactObj = ContactList.contactList.filter(function(obj){
